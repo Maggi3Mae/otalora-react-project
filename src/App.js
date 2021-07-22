46ec85df775c3926cf808bc43a346c8ec5fdd1d2
MaggieMae <margarita.mae.483@gmail.com>
2021-07-21 19:18:31 -0500

    Instalacion de router dom y configuracion de los path pero no funciona

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import NavBar from './componentes/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './componentes/ItemListContainer';
 import ItemDetailContainer from './componentes/ItemDetailContainer';
-
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' //es as le camba el nombre
 
 function App() {
   let saludo = "Hola, estas entrando en la mejor tienda del mundo mundial"
   return (
-    <>
-      <NavBar/>  
-      <ItemListContainer greeting={saludo}/>
-      <ItemDetailContainer />
-    </>
+    <Router>
+        <NavBar />
+      <Switch>      
+        <Route exact path="/">
+           <ItemListContainer greeting={saludo}/>
+        </Route>  
+        <Route exact path='/category' component={ItemDetailContainer} />
+      </Switch>
+    </Router>
   );
 }
 
